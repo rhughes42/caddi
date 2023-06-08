79cc20f2b8ad2d91b25d1923f3e91e2a4af19b5e
Ryan Hughes <rhu@xbuild.com>
2023-06-09 00:24:28 +0200

    Added firebase config.

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK5q1CZXQ0aNagOcSmaASxkrnfuv-WLvo",
+  authDomain: "caddi-app.firebaseapp.com",
+  projectId: "caddi-app",
+  storageBucket: "caddi-app.appspot.com",
+  messagingSenderId: "902211808497",
+  appId: "1:902211808497:web:10f826949b1f221273cd50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
